75186d32c516bc7a0daecc619ede0b31106b015c
Paul Nowoczynski <pnowoczynski@ddn.com>
2017-09-02 13:16:28 -0400

    Add different I/O patterns and test contexts
    Add initial suppport for sequential, random, and strided I/O patters
    with backwards support for sequential (and strided).  Note that this
    patch currenly only supports local I/O operations by the ranks and that
    strided mode is not yet implemented.
    
    A new structure called, yapio_test_ctx_t, is added to describe the
    properties of an I/O test.  yapio_test_ctx_t holds an array of
    yapio_blk_md_t pointers which hold arrays of block-wise I/O operations
    to be performed by this rank.  ytc_skip_mpi_gather mode, which forces
    all I/O's to be localized to each rank is the only mode implemented.
    
    Neither ytc_leave_holes and ytc_no_fsync have been implemented yet.
    
    yapioSinkBlkMd has been replaced by yapio_test_ctx_t::ytc_ops_md.
    
    yapio_rw() has been replaced with yapio_perform_io(), which takes a
    yapio_test_ctx_t as its sole parameter.
    
    The random test support has been implemented here.  It works by creating
    an array of random data from /dev/urandom and using the array elements
    as swap indexes.

--- yapio.c
+++ yapio.c
@@ -43,6 +43,13 @@ enum yapio_log_levels
     YAPIO_LL_MAX
 };
 
+enum yapio_patterns
+{
+    YAPIO_IOP_SEQUENTIAL,
+    YAPIO_IOP_RANDOM,
+    YAPIO_IOP_STRIDED,
+};
+
 #define YAPIO_EXIT_OK  0
 #define YAPIO_EXIT_ERR 1
 
@@ -80,8 +87,19 @@ typedef struct yapio_blk_metadata
     size_t ybm_blk_number;      //number of the block
 } yapio_blk_md_t;
 
-yapio_blk_md_t *yapioSourceBlkMd; //metadata which this rank maintains
-yapio_blk_md_t *yapioSinkBlkMd;   //metadata pushed from others to this rank
+yapio_blk_md_t  *yapioSourceBlkMd; //metadata which this rank maintains
+
+typedef struct yapio_test_context
+{
+    bool                ytc_leave_holes;      //some writes are skipped
+    bool                ytc_backwards;        //IOs are done in reverse order
+    bool                ytc_skip_mpi_gather;  //tests local buffers only
+//    bool                ytc_no_fsync;
+    bool                ytc_write;
+    enum yapio_patterns ytc_io_pattern;       //IO pattern to be employed
+    yapio_blk_md_t    **ytc_ops_md;           //operations metadata per block
+    int                *ytc_num_ops_per_rank; //count for ytc_ops_md arrays
+} yapio_test_ctx_t;
 
 static const char *
 yapio_ll_to_string(enum yapio_log_levels yapio_ll)
@@ -288,8 +306,7 @@ yapio_destroy_buffers(void)
 {
     if (yapioSourceBlkMd)
         free(yapioSourceBlkMd);
-    if (yapioSinkBlkMd)
-        free(yapioSinkBlkMd);
+
     if (yapioIOBuf)
         free(yapioIOBuf);
 }
@@ -308,10 +325,6 @@ yapio_alloc_buffers(void)
     if (yapioSourceBlkMd == NULL)
         yapio_destroy_buffers_and_abort();
 
-    yapioSinkBlkMd = calloc(yapioNumBlksPerRank, sizeof(yapio_blk_md_t));
-    if (yapioSinkBlkMd == NULL)
-        yapio_destroy_buffers_and_abort();
-
     yapioIOBuf = calloc(1, yapioBlkSz);
     if (yapioIOBuf == NULL)
         yapio_destroy_buffers_and_abort();
@@ -325,10 +338,6 @@ yapio_initialize_source_md_buffer(void)
     {
         yapioSourceBlkMd[i].ybm_writer_rank = yapioMyRank;
         yapioSourceBlkMd[i].ybm_blk_number =
-            (yapioMyRank * yapioNumBlksPerRank) + i;
-
-        yapioSinkBlkMd[i].ybm_writer_rank = yapioMyRank;
-        yapioSinkBlkMd[i].ybm_blk_number =
             (yapioMyRank * yapioNumBlksPerRank) + i;
     }
 }
@@ -397,61 +406,76 @@ yapio_get_rw_offset(const yapio_blk_md_t *md, size_t blk_sz)
  *    implicitly assigned set of contiguous blocks.
  */
 static int
-yapio_rw(bool write)
+yapio_perform_io(yapio_test_ctx_t *ytc)
 {
     int rc = 0;
+    int i;
 
     MPI_Barrier(MPI_COMM_WORLD);
-    size_t i;
-    for (i = 0; i < yapioNumBlksPerRank; i++)
+
+    for (i = 0; i < yapioNumRanks; i++)
     {
-        const yapio_blk_md_t *md = &yapioSinkBlkMd[i];
+        int rank_idx = ytc->ytc_backwards ? (yapioNumRanks - i - 1) : i;
 
-        if (write)
-            yapio_apply_contents_to_io_buffer(yapioIOBuf, yapioBlkSz, md);
+        const yapio_blk_md_t *md_array = ytc->ytc_ops_md[rank_idx];
+        if (!md_array)
+            continue;
 
-        /* Obtain this IO's offset from the
-         */
-        off_t off = yapio_get_rw_offset(md, yapioBlkSz);
-        if (off < 0)
+        int j;
+        for (j = 0; j < ytc->ytc_num_ops_per_rank[rank_idx]; j++)
         {
-            log_msg(YAPIO_LL_ERROR, "yapio_get_rw_offset() failed");
-            rc = -ERANGE;
-            break;
-        }
+            const yapio_blk_md_t *md = &md_array[j];
 
-        ssize_t io_rc, io_bytes = 0;
-        do
-        {
-            char *adjusted_buf = yapioIOBuf + io_bytes;
-            size_t adjusted_io_len = yapioBlkSz - io_bytes;
-            off_t adjusted_off = off + io_bytes;
+            if (ytc->ytc_write)
+                yapio_apply_contents_to_io_buffer(yapioIOBuf, yapioBlkSz, md);
 
-            io_rc = write ?
-                pwrite(yapioFileDesc, adjusted_buf, adjusted_io_len,
-                       adjusted_off) :
-                pread(yapioFileDesc, adjusted_buf, adjusted_io_len,
-                      adjusted_off);
-
-            if (io_rc > 0)
-                io_bytes += io_rc;
-
-        } while (io_rc > 0 && io_bytes < yapioBlkSz);
-
-        if (io_rc < 0)
-        {
-            log_msg(YAPIO_LL_ERROR, "io failed at offset %lu: %s",
-                    off, strerror(errno));
-            rc = -errno;
-            break;
-        }
-
-        if (!write)
-        {
-            rc =
-                yapio_verify_contents_of_io_buffer(yapioIOBuf, yapioBlkSz, md);
-            if (rc)
+            /* Obtain this IO's offset from the
+             */
+            off_t off = yapio_get_rw_offset(md, yapioBlkSz);
+            if (off < 0)
+            {
+                log_msg(YAPIO_LL_ERROR, "yapio_get_rw_offset() failed");
+                rc = -ERANGE;
                 break;
+            }
+
+            ssize_t io_rc, io_bytes = 0;
+            do
+            {
+                char *adjusted_buf = yapioIOBuf + io_bytes;
+                size_t adjusted_io_len = yapioBlkSz - io_bytes;
+                off_t adjusted_off = off + io_bytes;
+
+                io_rc = ytc->ytc_write ?
+                    pwrite(yapioFileDesc, adjusted_buf, adjusted_io_len,
+                           adjusted_off) :
+                    pread(yapioFileDesc, adjusted_buf, adjusted_io_len,
+                          adjusted_off);
+
+                log_msg(YAPIO_LL_DEBUG, "%s rc=%zd %lu",
+                        ytc->ytc_write ? "pwrite" : "pread", io_rc,
+                        adjusted_off);
+
+                if (io_rc > 0)
+                    io_bytes += io_rc;
+
+            } while (io_rc > 0 && io_bytes < yapioBlkSz);
+
+            if (io_rc < 0)
+            {
+                log_msg(YAPIO_LL_ERROR, "io failed at offset %lu: %s",
+                        off, strerror(errno));
+                rc = -errno;
+                break;
+            }
+
+            if (!ytc->ytc_write)
+            {
+                rc = yapio_verify_contents_of_io_buffer(yapioIOBuf, yapioBlkSz,
+                                                        md);
+                if (rc)
+                    break;
+            }
         }
     }
 
@@ -494,13 +518,169 @@ yapio_unlink_test_file(void)
 
             yapio_exit(YAPIO_EXIT_ERR);
         }
+
         log_msg(YAPIO_LL_DEBUG, "%s", yapioTestFileName);
     }
 }
 
+static void
+yapio_test_ctx_release(yapio_test_ctx_t *ytc)
+{
+    if (ytc->ytc_ops_md)
+    {
+        int i;
+        for (i = 0; i < yapioNumRanks; i++)
+            if (ytc->ytc_ops_md[i])
+                free(ytc->ytc_ops_md[i]);
+
+        free(ytc->ytc_ops_md);
+    }
+
+    if (ytc->ytc_num_ops_per_rank)
+        free(ytc->ytc_num_ops_per_rank);
+}
+
+static int
+yapio_read_from_dev_urandom(void *buffer, size_t size)
+{
+    int fd = open("/dev/urandom", O_RDONLY);
+    if (fd < 0)
+    {
+        fd = -errno;
+        log_msg(YAPIO_LL_ERROR, "open: %s", strerror(errno));
+        return fd;
+    }
+
+    int error = 0;
+    size_t nbytes = 0;
+    do
+    {
+        ssize_t rc = read(fd, ((char *)buffer) + nbytes, size - nbytes);
+        if (rc < 0)
+        {
+            error = -errno;
+            log_msg(YAPIO_LL_ERROR, "read: %s", strerror(errno));
+            break;
+        }
+
+        nbytes += rc;
+
+    } while (nbytes < size);
+
+    close(fd);
+    return error;
+}
+
+/**
+ * yapio_test_context_setup - Allocates memory buffers for the test according
+ *   to the input data in the yapio_test_ctx_t.  After memory allocation, this
+ *   function will arrange the yapio_blk_md_t either autonomously or in
+ *   conjunction with the other ranks in the job.  On completion, the 'ytc'
+ *   will have an array of pointers stored in ytc->ytc_ops_md which hold the
+ *   set of operations to be performed by this rank.
+ */
+static int
+yapio_test_context_setup(yapio_test_ctx_t *ytc)
+{
+    ytc->ytc_ops_md = calloc(yapioNumRanks, sizeof(yapio_blk_md_t *));
+    if (!ytc->ytc_ops_md)
+        return -ENOMEM;
+
+    ytc->ytc_num_ops_per_rank = calloc(yapioNumRanks, sizeof(int));
+    if (!ytc->ytc_num_ops_per_rank)
+    {
+        yapio_test_ctx_release(ytc);
+        return -ENOMEM;
+    }
+
+    if (ytc->ytc_skip_mpi_gather)
+    {
+        /* In this mode all operation instructions are pulled from the local
+         * Source Blk Metadata - there is no exchange with other ranks.
+         */
+        ytc->ytc_num_ops_per_rank[yapioMyRank] = yapioNumBlksPerRank;
+        ytc->ytc_ops_md[yapioMyRank] = calloc(yapioNumBlksPerRank,
+                                              sizeof(yapio_blk_md_t));
+
+        yapio_blk_md_t *md = ytc->ytc_ops_md[yapioMyRank];
+        if (!md)
+        {
+            yapio_test_ctx_release(ytc);
+            return -ENOMEM;
+        }
+
+        if (ytc->ytc_io_pattern == YAPIO_IOP_STRIDED)
+        {
+            yapio_test_ctx_release(ytc);
+            return -EINVAL;
+        }
+        else if (ytc->ytc_io_pattern == YAPIO_IOP_SEQUENTIAL)
+        {
+            int i;
+            for (i = 0; i < yapioNumBlksPerRank; i++)
+            {
+                int src_idx = i;
+
+                if (ytc->ytc_backwards)
+                    src_idx = (yapioNumBlksPerRank - 1 - i);
+
+                md[i] = yapioSourceBlkMd[src_idx];
+                log_msg(YAPIO_LL_TRACE, "rank=%d md->ybm_blk_number=%zd",
+                        md[i].ybm_writer_rank, md[i].ybm_blk_number);
+            }
+        }
+        else if (ytc->ytc_io_pattern == YAPIO_IOP_RANDOM)
+        {
+            size_t buf_sz = yapioNumBlksPerRank * sizeof(int);
+            int *array_of_randoms = malloc(buf_sz);
+            if (!array_of_randoms)
+            {
+                yapio_test_ctx_release(ytc);
+                return -ENOMEM;
+            }
+
+            int rc = yapio_read_from_dev_urandom((void *)array_of_randoms,
+                                                 buf_sz);
+            if (rc)
+            {
+                yapio_test_ctx_release(ytc);
+                return rc;
+            }
+
+            int i;
+            for (i = 0; i < yapioNumBlksPerRank; i++)
+                md[i] = yapioSourceBlkMd[i];
+
+            for (i = 0; i < yapioNumBlksPerRank; i++)
+            {
+                int swap_idx = array_of_randoms[i] % yapioNumBlksPerRank;
+                yapio_blk_md_t md_tmp = md[swap_idx];
+                md[swap_idx] = md[i];
+                md[i] = md_tmp;
+
+                log_msg(YAPIO_LL_TRACE, "swapped %d:%zu <-> %d:%zu",
+                        i, md[swap_idx].ybm_blk_number, swap_idx,
+                        md[i].ybm_blk_number)
+            }
+        }
+    }
+    return 0;
+}
+
+#define YAPIO_SIMPLE_IO_TEST(ctx, write)                            \
+    {                                                               \
+        (ctx)->ytc_leave_holes     = false;                         \
+        (ctx)->ytc_backwards       = false;                         \
+        (ctx)->ytc_skip_mpi_gather = true;                          \
+        (ctx)->ytc_write           = write;                         \
+        (ctx)->ytc_io_pattern      = YAPIO_IOP_SEQUENTIAL;          \
+    }
+
 int
 main(int argc, char *argv[])
 {
+    yapio_test_ctx_t my_test_ctx;
+
     yapio_mpi_setup(argc, argv);
     yapio_getopts(argc, argv);
 
@@ -508,8 +688,26 @@ main(int argc, char *argv[])
 
     yapio_setup_test_file();
 
-    yapio_rw(true);
-    yapio_rw(false);
+    YAPIO_SIMPLE_IO_TEST(&my_test_ctx, true);
+    my_test_ctx.ytc_io_pattern = YAPIO_IOP_RANDOM;
+//    my_test_ctx.ytc_backwards = true;
+    int rc = yapio_test_context_setup(&my_test_ctx);
+    if (rc)
+        yapio_exit(YAPIO_EXIT_ERR);
+
+    yapio_perform_io(&my_test_ctx);
+
+    yapio_test_ctx_release(&my_test_ctx);
+
+    YAPIO_SIMPLE_IO_TEST(&my_test_ctx, false);
+    my_test_ctx.ytc_io_pattern = YAPIO_IOP_RANDOM;
+//    my_test_ctx.ytc_backwards = true;
+    rc = yapio_test_context_setup(&my_test_ctx);
+    if (rc)
+        yapio_exit(YAPIO_EXIT_ERR);
+
+    yapio_perform_io(&my_test_ctx);
+    yapio_test_ctx_release(&my_test_ctx);
 
     yapio_close_test_file();
 
